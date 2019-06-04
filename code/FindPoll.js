983e98c5cdfd502840073109c89cd2f91667c574
Josiah Ruddell <jruddell@gmail.com>
2019-06-04 15:10:08 -0700

    Views + Voting modeling

--- code/FindPoll.js
+++ code/FindPoll.js
@@ -1,6 +1,6 @@
 function FindPoll(search) {
   // todo: fuzzy search poll names ${search}
-  return ['Awesome poll 1']
+  return ['What is the name of the song that gets stuck in your head from The Lego Movie?']
 }
 
 module.exports.function = FindPoll
---
+++ code/RecordVote.js
@@ -0,0 +1,9 @@
+var console = require('console')
+
+function RecordVote(poll, option, $vivContext) {
+  // todo: send api to update poll
+  // return the poll with the current user vote
+  return poll
+}
+
+module.exports.function = RecordVote
