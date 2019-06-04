f54f1bb3740bda4f25d8babd5e89d9f1fb7db98e
Josiah Ruddell <jruddell@gmail.com>
2019-06-04 10:42:17 -0700

    View poll permissions checked error

--- code/FetchPollDetails.js
+++ code/FetchPollDetails.js
@@ -1,7 +1,15 @@
-function FetchPollDetails(name) {
-  // todo: get the poll by name ${name}
-  return {
+var fail = require('fail')
+var console = require('console')
+
+function FetchPollDetails(name, $vivContext) {
+  // prefix so we know this ID came from bixby
+  const userId = 'bixby:' + $vivContext.userId
+
+  // todo: get the poll by name ${name} & ${username}
+  
+  const poll = {
     name: name,
+    vote: [1],
     options: [{
         text: 'Everything is awesome',
         votes: 10,
@@ -19,6 +27,12 @@ function FetchPollDetails(name) {
     ],
     totalVotes: 10
   }
+
+  if (!poll.vote || poll.vote.length === 0) {
+    throw fail.checkedError("User hasn't voted", "user-has-not-voted")
+  }
+
+  return poll
 }
 
 module.exports.function = FetchPollDetails
--- code/FindPoll.js
+++ code/FindPoll.js
@@ -1,6 +1,6 @@
 function FindPoll(search) {
   // todo: fuzzy search poll names ${search}
-  return ['Awesome poll 1', 'Awesome poll 2']
+  return ['Awesome poll 1']
 }
 
 module.exports.function = FindPoll
